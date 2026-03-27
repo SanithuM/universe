06890cd9bffa674d481399d446f249e3582f5a88
Sanithu Malhiru <easanithumalhiru@gmail.com>
2026-03-28 00:54:41 +0530

    fix: update CORS configuration to include production URL

--- server/index.js
+++ server/index.js
@@ -21,7 +21,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  'http://localhost:5173',
+  'https://universe-nvhg.onrender.com'
+));
 app.use(express.json({ limit: '50mb' })); //Allow to parse JSON bodies
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api/upload', require('./routes/upload'));
@@ -30,7 +33,7 @@ app.use('/api/upload', require('./routes/upload'));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.CLIENT_URL || "http://localhost:3000", "http://localhost:5173" ],// React frontend URL
+    origin: [process.env.CLIENT_URL || "http://localhost:3000", "http://localhost:5173", "https://universe-nvhg.onrender.com" ],// React frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
   }
 });
