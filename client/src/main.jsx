9a3776e0e30b5efc7c4135a059b705b76a8d0477
Sanithu Malhiru <easanithumalhiru@gmail.com>
2026-03-31 12:39:03 +0530

    feat: wrap GoogleOAuthProvider with React.StrictMode for improved development checks

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,9 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-    <App />
-  </GoogleOAuthProvider>
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
 )
